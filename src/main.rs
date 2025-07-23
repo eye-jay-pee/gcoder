1c8362afdadfb923160dab2a47a33bd39480e0b6
Isaac Peterson <isaacpeterson2017@gmail.com>
2025-07-23 12:53:14 -0500

    cleaned it up a lil bit for em

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,43 @@
-use std::io::{Write,Read};
+use serialport::{SerialPort, TTYPort};
+//use std::error::Error;
+use std::io::{Read, Write};
 use std::time::Duration;
-use serialport::SerialPort;
 
-fn reset_serial_connection(port: &mut dyn SerialPort) {
-    port.write_data_terminal_ready(true).unwrap(); 
-    port.write_data_terminal_ready(false).unwrap();
-}
-
-fn main() {
-    let port_name = "/dev/ttyACM0";
-    let baud_rate = 115200;
-    let timeout = Duration::from_secs(30);
-
-    let mut port = match serialport::new(port_name, baud_rate)
+fn serial_starter(device: &str, baud: u32, timeout: Duration) -> TTYPort {
+    let mut port: serialport::TTYPort = serialport::new(device, baud)
         .timeout(timeout)
-        .open() {
-            Ok(mut p) => {
-                p.write_request_to_send(true).unwrap(); 
-                reset_serial_connection(&mut *p);
-                p
-            },
-            Err(e) => {
-                eprintln!("failed to open serial connection {} at {} baud, becasue of {}",
-                    port_name, baud_rate, e);
-                return;
-            }
-        };
+        .open_native()
+        .expect("couldn't open");
 
-
-    let cmd = "M115\n";
-    if let Err(e) = port.write_all(cmd.as_bytes()) {
-        eprintln!("failed to send \n{} because of {}", cmd, e);
-        return;
-    } else {
-        println!("sent {} to {}", cmd, port_name);
+    port.write_data_terminal_ready(false).unwrap();
+    port.write_request_to_send(true).unwrap();
+    port
+}
+fn send_cmd(mut port: TTYPort, cmd: &str) {
+    match port.write_all(cmd.as_bytes()) {
+        () => println!("sent:{}", cmd),
+        Err(e) => eprintln!("failed to send \n{} because of {}", cmd, e),
     }
-
+}
+fn recive_ack(port: TTYPort) {
     let mut buf = [0u8; 1024];
     match port.read(&mut buf) {
-        Ok(n) => {
-            println!("Received {} bytes:", n);
-            println!("{}", String::from_utf8_lossy(&buf[..n]));
-        }
-        Err(e) => {
-            eprintln!("Response not recived from {} due to {}", port_name, e);
-        }
+        Ok(n) => println!("printer:{}", String::from_utf8_lossy(&buf[..n])),
+        Err(e) => eprintln!("no ack recived from: {}", e),
     }
-
+}
+fn serial_killer(mut port: TTYPort) {
     port.flush().ok();
     port.write_request_to_send(false).ok(); // master is no longer listening
     drop(port);
+}
+
+fn main() {
+    let timeout = Duration::from_secs(8);
+    let mut port = serial_starter("/dev/ttyACM0", 115200, timeout);
+
+    send_cmd(port, "M115\n");
+    recive_ack(port);
+
+    serial_killer(port);
 }
