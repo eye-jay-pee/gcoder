f65b43e44b2daf263fdd6c89fc3a57c00c0e07be
isaac peterson <isaac@ijp.codes>
2025-08-17 19:30:32 -0500

    debugging serial disconenct thign

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,24 @@
-use std::io::{Write,Read};
-use std::time::Duration;
 use serialport::SerialPort;
+use std::io::{Read, Write};
+use std::time::Duration;
 
 fn reset_serial_connection(port: &mut dyn SerialPort) {
-    port.write_data_terminal_ready(true).unwrap(); 
+    port.write_data_terminal_ready(true).unwrap();
     port.write_data_terminal_ready(false).unwrap();
 }
 
 fn main() {
     let port_name = "/dev/ttyACM0";
     let baud_rate = 115200;
-    let timeout = Duration::from_secs(30);
+    let timeout = Duration::from_secs(1);
 
-    let mut port = match serialport::new(port_name, baud_rate)
+    let mut port = serialport::new(port_name, baud_rate)
         .timeout(timeout)
-        .open() {
-            Ok(mut p) => {
-                p.write_request_to_send(true).unwrap(); 
-                reset_serial_connection(&mut *p);
-                p
-            },
-            Err(e) => {
-                eprintln!("failed to open serial connection {} at {} baud, becasue of {}",
-                    port_name, baud_rate, e);
-                return;
-            }
-        };
+        .open()
+        .unwrap();
 
+    port.write_request_to_send(true).unwrap();
+    reset_serial_connection(&mut *port);
 
     let cmd = "M115\n";
     if let Err(e) = port.write_all(cmd.as_bytes()) {
