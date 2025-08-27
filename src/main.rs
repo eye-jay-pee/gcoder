af24bac6aa695046933d9c5e151a8d8433578791
isaac peterson <isaac@ijp.codes>
2025-08-27 13:24:15 -0500

    syntax cleanup part 1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::io::{Read, Write};
-use std::thread;
 use std::time::Duration;
 
 fn main() {
@@ -8,27 +6,46 @@ fn main() {
     let timeout = Duration::from_secs(1);
     let cmd = "M115\n";
 
-    let mut buf = [0u8; 256];
+    let mut port = serial::initalize(port_name, baud_rate, timeout).unwrap();
 
-    rebind();
-    let mut port = serialport::new(port_name, baud_rate)
-        .timeout(timeout)
-        .open()
-        .unwrap();
-
-    port.write_all(cmd.as_bytes()).unwrap();
-    println!("sent {} to {}", cmd, port_name);
-
-    let n = port.read(&mut buf).unwrap();
-    println!("Received {} bytes:", n);
-    println!("{}", String::from_utf8_lossy(&buf[..n]));
+    println!("{}:\n{}", "host", cmd);
+    let reply = serial::transfer(&mut *port, cmd).unwrap();
+    println!("{}:\n{}", "printer", reply);
 }
 
-fn rebind() {
-    use std::fs;
+mod serial {
+    use serialport::{Result, SerialPort};
+    use std::time::Duration;
 
-    let iface = "1-1.3:1.0"; // your interface from dmesg/udev
-    fs::write("/sys/bus/usb/drivers/cdc_acm/unbind", iface).unwrap();
-    thread::sleep(Duration::from_millis(200));
-    fs::write("/sys/bus/usb/drivers/cdc_acm/bind", iface).unwrap();
+    pub struct _Printer {
+        _port: Option<Box<dyn SerialPort>>,
+    }
+
+    pub fn transfer(port: &mut dyn SerialPort, data: &str) -> Result<String> {
+        let mut buf = [0u8; 256];
+
+        port.write_all(data.as_bytes())?;
+        let n = port.read(&mut buf)?;
+
+        Ok(String::from_utf8_lossy(&buf[..n]).into_owned())
+    }
+    pub fn initalize(
+        name: &str,
+        baud: u32,
+        timeout: Duration,
+    ) -> Result<Box<dyn SerialPort>> {
+        rebind()?;
+        Ok(serialport::new(name, baud).timeout(timeout).open()?)
+    }
+
+    fn rebind() -> std::io::Result<()> {
+        use std::time::Duration;
+        use std::{fs, thread};
+
+        let iface = "1-1.3:1.0"; // your interface from dmesg/udev
+        fs::write("/sys/bus/usb/drivers/cdc_acm/unbind", iface)?;
+        thread::sleep(Duration::from_millis(200));
+        fs::write("/sys/bus/usb/drivers/cdc_acm/bind", iface)?;
+        Ok(())
+    }
 }
